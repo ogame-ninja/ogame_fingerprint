c930a0720090ccfcf3ae7b4c2bb5ffdc0c1b8ddb
notriv <notriv@ogame.ninja>
2023-02-09 00:49:35 -0800

    cleanup

--- game1.js
+++ game1.js
@@ -1147,87 +1147,60 @@ const getBrowserRenderingEngine = uaName => {
 
 const getOSInfo = () => {
         try {
-            const osArr = [{
-                    's': 'Windows 10',
-                    'r': /(Windows 10.0|Windows NT 10.0)/
-                }, {
-                    's': 'Windows 8.1',
-                    'r': /(Windows 8.1|Windows NT 6.3)/
-                }, {
-                    's': 'Windows 8',
-                    'r': /(Windows 8|Windows NT 6.2)/
-                }, {
-                    's': 'Windows 7',
-                    'r': /(Windows 7|Windows NT 6.1)/
-                }, {
-                    's': 'Windows Vista',
-                    'r': /Windows NT 6.0/
-                }, {
-                    's': 'Windows Server 2003',
-                    'r': /Windows NT 5.2/
-                }, {
-                    's': 'Windows XP',
-                    'r': /(Windows NT 5.1|Windows XP)/
-                }, {
-                    's': 'Windows 2000',
-                    'r': /(Windows NT 5.0|Windows 2000)/
-                }, {
-                    's': 'Windows ME',
-                    'r': /(Win 9x 4.90|Windows ME)/
-                }, {
-                    's': 'Windows 98',
-                    'r': /(Windows 98|Win98)/
-                }, {
-                    's': 'Windows 95',
-                    'r': /(Windows 95|Win95|Windows_95)/
-                }, {
-                    's': 'Windows NT 4.0',
-                    'r': /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/
-                }, {
-                    's': 'Windows CE',
-                    'r': /Windows CE/
-                }, {
-                    's': 'Windows 3.11',
-                    'r': /Win16/
-                }, {
-                    's': 'Android',
-                    'r': /Android/
-                }, {
-                    's':'Open BSD',
-                    'r': /OpenBSD/
-                }, {
-                    's': 'Sun OS',
-                    'r': /SunOS/
-                }, {
-                    's': 'Chrome OS',
-                    'r': /CrOS/
-                }, {
-                    's': 'Linux',
-                    'r': /(Linux|X11(?!.*CrOS))/
-                }, {
-                    's': 'iOS',
-                    'r': /(iPhone|iPad|iPod)/
-                }, {
-                    's': 'Mac OS X',
-                    'r': /Mac OS X/
-                }, {
-                    's': 'Mac OS',
-                    'r': /(Mac OS|MacPPC|MacIntel|Mac_PowerPC|Macintosh)/
-                }, {
-                    's': 'QNX',
-                    'r': /QNX/
-                }, {
-                    's': 'UNIX',
-                    'r': /UNIX/
-                }, {
-                    's': 'BeOS',
-                    'r': /BeOS/
-                }, {
-                    's': 'OS/2',
-                    'r': /OS\/2/
-                }, {
-                    's': 'Search Bot',
-                    'r': /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma | ia_archiver) /
+            const osArr = [{'s': 'Windows 10',
+                            'r': /(Windows 10.0|Windows NT 10.0)/ }, {
+                            's': 'Windows 8.1',
+                            'r': /(Windows 8.1|Windows NT 6.3)/ }, {
+                            's': 'Windows 8',
+                            'r': /(Windows 8|Windows NT 6.2)/ }, {
+                            's': 'Windows 7',
+                            'r': /(Windows 7|Windows NT 6.1)/ }, {
+                            's': 'Windows Vista',
+                            'r': /Windows NT 6.0/ }, {
+                            's': 'Windows Server 2003',
+                            'r': /Windows NT 5.2/ }, {
+                            's': 'Windows XP',
+                            'r': /(Windows NT 5.1|Windows XP)/ }, {
+                            's': 'Windows 2000',
+                            'r': /(Windows NT 5.0|Windows 2000)/ }, {
+                            's': 'Windows ME',
+                            'r': /(Win 9x 4.90|Windows ME)/ }, {
+                            's': 'Windows 98',
+                            'r': /(Windows 98|Win98)/ }, {
+                            's': 'Windows 95',
+                            'r': /(Windows 95|Win95|Windows_95)/ }, {
+                            's': 'Windows NT 4.0',
+                            'r': /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ }, {
+                            's': 'Windows CE',
+                            'r': /Windows CE/ }, {
+                            's': 'Windows 3.11',
+                            'r': /Win16/ }, {
+                            's': 'Android',
+                            'r': /Android/ }, {
+                            's':'Open BSD',
+                            'r': /OpenBSD/ }, {
+                            's': 'Sun OS',
+                            'r': /SunOS/ }, {
+                            's': 'Chrome OS',
+                            'r': /CrOS/ }, {
+                            's': 'Linux',
+                            'r': /(Linux|X11(?!.*CrOS))/ }, {
+                            's': 'iOS',
+                            'r': /(iPhone|iPad|iPod)/ }, {
+                            's': 'Mac OS X',
+                            'r': /Mac OS X/ }, {
+                            's': 'Mac OS',
+                            'r': /(Mac OS|MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ }, {
+                            's': 'QNX',
+                            'r': /QNX/ }, {
+                            's': 'UNIX',
+                            'r': /UNIX/ }, {
+                            's': 'BeOS',
+                            'r': /BeOS/ }, {
+                            's': 'OS/2',
+                            'r': /OS\/2/ }, {
+                            's': 'Search Bot',
+                            'r': /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma | ia_archiver) /
             }];
         let _0x120cdc;
         let _0x5e8ac4 = osArr.filter(osItem => osItem['r']['test'](navigator.userAgent))[0]['s'];
@@ -1784,11 +1757,11 @@ function decrypt(arg1) {
 }
 
 let game1 = async function(clb, arg2 = null) {
-    let nowTimestamp = new Date().getTime(),
-        xGame = localStorage.getItem('x-game'),
-        randCharFn = () => String.fromCharCode(32 + Math.random() * 94 | 0),
-        part1CharsArr,
-        part2Int;
+    let nowTimestamp = new Date().getTime();
+    let xGame = localStorage.getItem('x-game');
+    let randCharFn = () => String.fromCharCode(32 + Math.random() * 94 | 0);
+    let part1CharsArr;
+    let part2Int;
     try {
         let xVec = localStorage.getItem("x-vec");
         let spaceIdx = xVec.lastIndexOf(' ');
